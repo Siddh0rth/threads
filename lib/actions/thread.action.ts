d1c3e3a3dc6340503210889f852088bbd35db10e
Siddh0rth <siddharthy1236@gmail.com>
2023-10-13 13:33:19 +0530

    threadcard and post a comment to thread is done

--- lib/actions/thread.action.ts
+++ lib/actions/thread.action.ts
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 
 import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
@@ -6,32 +6,74 @@ import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
 interface Params {
-    text: string,
-    author: string,
-    communityId: string | null,
-    path: string,
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
+}
+
+export async function createThread({
+  text,
+  author,
+  communityId,
+  path,
+}: Params) {
+  try {
+    connectToDB();
+
+    const createdThread = await Thread.create({
+      text,
+      author,
+      community: null,
+    });
+
+    //update user model
+    //update community model
+    await User.findByIdAndUpdate(author, {
+      $push: { threads: createThread._id },
+    });
+
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Error creating thread: ${error.message}`);
   }
+}
 
-export async function createThread({ text, author, communityId, path }: Params) {
-    try {
-        connectToDB()
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
 
-        const createdThread = await Thread.create({
-            text,
-            author,
-            community: null,
-        })
+  //pagination -> to on which page we are
+  // Calculate the number of posts to skip based on the page number and page size.
+  const skipAmount = (pageNumber - 1) * pageSize;
 
-        //update user model
-        //update community model
-        await User.findByIdAndUpdate(author, {
-            $push: { threads: createThread._id }
-        })
+  // Create a query to fetch the posts that have no parent (top-level threads) (a thread that is not a comment/reply).
+  //Fetch the posts that have no parents (top-level threads...)
+  const postQuery = Thread.find({
+    parentId: { $in: [null, undefined] },
+  })
+    .sort({
+      createdAt: "desc",
+    })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: "author", model: User })
+    .populate({
+      path: "children", // Populate the children field
+      populate: {
+        path: "author", // Populate the author field within children
+        model: User,
+        select: "_id name parentId image", // Select only _id and username fields of the author
+      },
+    });
 
-        revalidatePath(path);
-    } catch (error: any) {
-        throw new Error(`Error creating thread: ${error.message}`)
-    }
+  // Count the total number of top-level posts (threads) i.e., threads that are not comments.
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  }); // Get the total count of posts
+  
+  const posts = await postQuery.exec();
 
-    
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
 }
