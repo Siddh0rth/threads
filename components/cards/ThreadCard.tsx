6cd9012c97edaea76ddeb369da672ee25a556003
Siddh0rth <siddharthy1236@gmail.com>
2023-10-31 19:33:35 +0530

    profile page is completed

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -36,83 +36,86 @@ const ThreadCard = ({
   comments,
   isComment,
 }: Props) => {
-        return (
-            <article className={`flex w-full flex-col rounded-xl ${isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"
-              }`}>
-                <div className="flex items-start justify-between">
-                    <div className="flex w-full flex-1 flex-row gap-4">
-                        {/* this div show's the profile image  */}
-                        <div className="flex flex-col items-center">
-                            <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
-                                <Image
-                                    src={author.image}
-                                    alt="Profile Image"
-                                    fill
-                                    className="cursor-pointer rounded-full" />
-                            </Link>
-                            {/* below div add's the line below the profile image and show's the comment's */}
-                            <div className="thread-card_bar" />
-                        </div>
+  return (
+    <article
+      className={`flex w-full flex-col rounded-xl ${
+        isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"
+      }`}>
+      <div className="flex items-start justify-between">
+        <div className="flex w-full flex-1 flex-row gap-4">
+          {/* this div show's the profile image  */}
+          <div className="flex flex-col items-center">
+            <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
+              <Image
+                src={author.image}
+                alt="Profile Image"
+                fill
+                className="cursor-pointer rounded-full"
+              />
+            </Link>
+            {/* below div add's the line below the profile image and show's the comment's */}
+            <div className="thread-card_bar" />
+          </div>
 
-                        <div className="flex w-full flex-col">
-                            <Link href={`/profile/${author.id}`} className="w-fit">
-                                <h4 className="cursor-pointer text-base-semibold text-light-1">
-                                    {author.name}
-                                </h4>
-                            </Link>
+          <div className="flex w-full flex-col">
+            <Link href={`/profile/${author.id}`} className="w-fit">
+              <h4 className="cursor-pointer text-base-semibold text-light-1">
+                {author.name}
+              </h4>
+            </Link>
 
-                            {/* this p tag is for the content which is writen by the user */}
-                            <p className="mt-2 text-small-regular text-light-2">{content}</p>
+            {/* this p tag is for the content which is writen by the user */}
+            <p className="mt-2 text-small-regular text-light-2">{content}</p>
 
-                            {/* this div is for the like and share */}
-                            <div className="mt-5 flex flex-col gap-3">
-                                <div className="flex gap-3.5">
-                                    <Image
-                                        src="/assets/heart-gray.svg"
-                                        alt="heart"
-                                        width={24}
-                                        height={24}
-                                        className="cursor-pointer object-contain"
-                                    />
-                                    <Link href={`/thread/${id}`}>
-                                        <Image
-                                            src="/assets/reply.svg"
-                                            alt="reply"
-                                            width={24}
-                                            height={24}
-                                            className="cursor-pointer object-contain"
-                                        />
-                                    </Link>
-                                    <Image
-                                        src="/assets/repost.svg"
-                                        alt="repost"
-                                        width={24}
-                                        height={24}
-                                        className="cursor-pointer object-contain"
-                                    />
-                                    <Image
-                                        src="/assets/share.svg"
-                                        alt="share"
-                                        width={24}
-                                        height={24}
-                                        className="cursor-pointer object-contain"
-                                    />
-                                </div>
+            {/* this div is for the like and share */}
+            <div className={`${isComment && "mb-10"} mt-5 flex flex-col gap-3`}>
+              <div className="flex gap-3.5">
+                <Image
+                  src="/assets/heart-gray.svg"
+                  alt="heart"
+                  width={24}
+                  height={24}
+                  className="cursor-pointer object-contain"
+                />
+                <Link href={`/thread/${id}`}>
+                  <Image
+                    src="/assets/reply.svg"
+                    alt="heart"
+                    width={24}
+                    height={24}
+                    className="cursor-pointer object-contain"
+                  />
+                </Link>
+                <Image
+                  src="/assets/repost.svg"
+                  alt="heart"
+                  width={24}
+                  height={24}
+                  className="cursor-pointer object-contain"
+                />
+                <Image
+                  src="/assets/share.svg"
+                  alt="heart"
+                  width={24}
+                  height={24}
+                  className="cursor-pointer object-contain"
+                />
+              </div>
 
-                                {isComment && comments.length > 0 && (
-                                    <Link href={`/thread/${id}`}>
-                                        <p className="mt-1 text-subtle-medium">{comments.length}
-                                            replies
-                                        </p>
-                                    </Link>
-                                )}
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </article>
-        );
-    };
+              {isComment && comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className="mt-1 text-subtle-medium">
+                    {comments.length}
+                    replies
+                  </p>
+                </Link>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
+};
 
 export default ThreadCard;
